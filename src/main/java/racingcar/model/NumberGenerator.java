492056abe936f5a0674144ca5860f919fcc135ff
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-13 18:48:13 +0900

    feat 랜덤 수 획득 구현

---
+++ src/main/java/racingcar/model/NumberGenerator.java
@@ -0,0 +1,9 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberGenerator {
+    public static int createRandomNumber() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+}
