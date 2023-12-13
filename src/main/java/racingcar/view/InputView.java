395050a5433f3851887bc70c6aede332c4e5c867
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-13 18:16:08 +0900

    feat 입력 요청 메시지 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,7 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.constant.RequestMessage;
 import racingcar.validator.InputValidator;
 
 import java.util.Arrays;
@@ -14,6 +15,7 @@ public class InputView {
     }
 
     public List<String> requestName() {
+        System.out.println(RequestMessage.REQUEST_CARS_NAME);
         String s = Console.readLine();
         List<String> names = Arrays.stream(s.trim().split(",", -1)).toList();
         validator.validCarName(names);
